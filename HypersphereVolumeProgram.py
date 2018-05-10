61cd8e28b03c700cf733dd67f97f73f4eb7d5ddb
kookieBE <31085929+kookieBE@users.noreply.github.com>
2018-05-10 17:45:52 +0930

    Simplified code somewhat

--- HypersphereVolumeProgram.py
+++ HypersphereVolumeProgram.py
@@ -1,15 +1,8 @@
-#Simple program designed to find volume of n-ball in n dimensions
-
 import random
-import math
 
-n = 8 #number of dimensions
-x = 1000000 #number of points to be pinged
+n = 3 #number of dimensions
+x = 10000 #number of points to be pinged
 reps = 10 #number of repitions to be made
-
-#pointsPinged = [] #list of all points in space which have been made
-#lengths = [] #output list for magnitude function
-ratioResults = []
 
 def randPoint(): #defining function which returns a point with a random set of coordinates
   i=0
@@ -20,29 +13,27 @@ def randPoint(): #defining function which returns a point with a random set of c
   return point;
 
 def magnitude(list): #defining function which finds magnitude of vector drawn from origin to point
-  toBeSummed = []
+  sum = []
   for v in list:
-    toBeSummed.append((v)**2)
-  leng = math.sqrt(sum(toBeSummed))
+    sum.append((v)**2)
+  leng = (sum)**0.5
   return leng;
 
-def sort(list, writeTo): #function finds ratio
+def sort(list): #function finds ratio
 	for v in list:
 		if v <= 1:
-			writeTo[0] += 1;
-		writeTo[1] += 1;
-
-def listWriter(list):
-  fileOpened = open('./valueDump.txt', 'a') #change path for where to write program to
-  for r in list:
-    fileOpened.write(str(r)+'\n')
+			rat[0] += 1;
+	rat[1] = x;
+	return rat
 
 k=0
+ratio = []
 while k != reps:
   k += 1
   pingedPoints = []
   lengths = []
-  j=0 #while loop which makes 'x' number of points
+  
+  j=0
   while j!=x:
     j += 1
     pingedPoints.append(randPoint())
@@ -50,16 +41,8 @@ while k != reps:
   for p in pingedPoints:
     lengths.append(magnitude(p))
   indvRatio = [0,0]
-  sort(lengths, indvRatio)
+  indvRatio = sort(lengths)
   print(str(k)+' '+str(indvRatio))
-  ratioResults.append(indvRatio)
-#print(ratioResults)
+  ratio.append(indvRatio)
 
-#listWriter(ratioResults)
-
-
-#print(pingedPoints);
-#print("");
-#print(lengths);
-#print("");
-#print(ratio);
+#print(ratio)
